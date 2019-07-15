f3e0af25e89ce635b02b1a4feba99af0a18e52d4
dorara <satosinisemono@gmail.com>
2019-07-15 22:54:06 +0900

    add: スタイル当て

--- user.php
+++ user.php
@@ -2,71 +2,106 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>図書貸し出し</title>
+        <title>[太郎]さんの貸出画面</title>
+        <link rel="stylesheet" type="text/css" media="screen" href="./assets/main.css">
     </head>
     <body>
-        <?php
-        require_once('class.php');
-        session_start();
-        print '<a href="logout.php">ログアウト</a><br/>';
+        <div class="l-wrap">
+            <header class="l-header">
+                <div class="l-container">
+                    <p>図書貸出システム</p>
+                </div>
+            </header>
 
-        if(!isset($_SESSION['login'])){
-            print 'ログインしなおしてください';
-            print '<form>';
-            print '<input type="button" onclick="history.back()" value="戻る">';
-            print '</form>';
-            exit(1);
-        }
-
-        if(isset($_SESSION['error'])){
-            print $_SESSION['error'];
-            print '<form>';
-            print '<input type="button" onclick="history.back()" value="戻る">';
-            print '</form>';
-            exit(1);
-        }
-        if(isset($_SESSION['caution'])){
-            print $_SESSION['caution'];
-            print '<br/>';
-            print var_dump($_SESSION['rec']);
-        }
-        $user = unserialize($_SESSION['user']);
-        $rental = unserialize($_SESSION['rental']);
-        /*テスト
-        print var_dump($rental);
-        print '<br />'; 
-        print $rental->rental1->chkout;
-        print '<br />'; 
-        print $rental->rental1->book->bookpub;
-        */
-
-
-        ?>
-        <form method="post" action="check_user_class.php">
-            <br/>
-            <!-- レンタルの数だけ貸出明細情報を表示-->
             <?php
-            for($i=1;$i <= $rental->bookcount; $i++){
-                print $i.",";
-                print "図書番号:".$rental->{'rental'.$i}->bookcode.",";
-                print "タイトル:".$rental->{'rental'.$i}->book->booktitle.",";
-                print "期限:".$rental->{'rental'.$i}->schreturn.",";
-                if($rental->{'rental'.$i}->exthistory == 0){
-                    print "<input type=\"checkbox\" name=\"extends[]\" value=".$i.">".$i."を延長する";
-                }
-                print "<br/>";
-            }
-            while($i<=$rental->bookcount+$_SESSION['availablerental']){
-                print $i;
-                print "<input name=\"".$i."\" type=\"text\" style=\"width:100px\"><br/>";
-                $i++;
-            }
+            // require_once('class.php');
+            // session_start();
+            // print '<a href="logout.php">ログアウト</a><br/>';
+
+            // if(!isset($_SESSION['login'])){
+            //     print 'ログインしなおしてください';
+            //     print '<form>';
+            //     print '<input type="button" onclick="history.back()" value="戻る">';
+            //     print '</form>';
+            //     exit(1);
+            // }
+
+            // if(isset($_SESSION['error'])){
+            //     print $_SESSION['error'];
+            //     print '<form>';
+            //     print '<input type="button" onclick="history.back()" value="戻る">';
+            //     print '</form>';
+            //     exit(1);
+            // }
+            // if(isset($_SESSION['caution'])){
+            //     print $_SESSION['caution'];
+            //     print '<br/>';
+            //     print var_dump($_SESSION['rec']);
+            // }
+            // $user = unserialize($_SESSION['user']);
+            // $rental = unserialize($_SESSION['rental']);
+            /*テスト
+            print var_dump($rental);
+            print '<br />'; 
+            print $rental->rental1->chkout;
+            print '<br />'; 
+            print $rental->rental1->book->bookpub;
+            */
+
+
             ?>
 
+            <!-- pageContents -->
+            <div class="p-user l-content">
+                <div class="l-container">
+                    <h1>[太郎]さんの貸出画面</h1>
+                    <form method="post" action="check_user_class.php">
+                        <h2>貸出済み</h2>
+                        <!-- レンタルの数だけ貸出明細情報を表示-->
+                        <table>
+                            <tr>
+                                <th>id</th>
+                                <th>図書番号</th>
+                                <th>タイトル</th>
+                                <th>貸出期限</th>
+                                <th>延長</th>
+                            </tr>
+                            <?php
+                                for($i=1;$i <= $rental->bookcount; $i++){
+                                    $html = "";
+                                    $html = "<tr>";
+                                    $html = "<td>".$i."</td>";
+                                    $html = "<td>".$rental->{'rental'.$i}->bookcode."</td>";
+                                    $html = "<td>".$rental->{'rental'.$i}->book->booktitle."</td>";
+                                    $html = "<td>".$rental->{'rental'.$i}->schreturn."</td>";
+                                    if($rental->{'rental'.$i}->exthistory == 0){
+                                        $html = "<td><label><input type=\"checkbox\" name=\"extends[]\" value=".$i.">".$i."を延長する</label></td>";
+                                    }
+                                    $html = "</tr>";
+                                    print $html;
+                                }
+                            ?>
+                        </table>
 
-            <input name="url" type="hidden" value="confirm">
-            <br/>
-            <input type="submit" value="登録確認">
-        </form>
+                        <h2>新規に貸出</h2>
+                        <?php
+                            while($i<=$rental->bookcount+$_SESSION['availablerental']){
+                                print $i;
+                                print "<input name=\"".$i."\" type=\"text\">";
+                                $i++;
+                            }
+                        ?>
+
+
+                        <input name="url" type="hidden" value="confirm">
+                        <input type="submit" value="この内容で登録" class="c-submit">
+                    </form>
+                    
+                    <div class="logout">
+                        <a href="./input.html">検索画面に戻る</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
